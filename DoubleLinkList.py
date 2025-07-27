71c4b6c72f81c2074c543108c10dc8a833a03f0a
Behroz kisana <behrozgujjar16@gmail.com>
2025-07-28 00:26:25 +0500

    Add code for double link list it is additional code

--- DoubleLinkList.py
+++ DoubleLinkList.py
@@ -143,9 +143,152 @@ dll.insert(11,0)
 dll.insert(32,1)
 print([node.value for node in dll])
 
-dll.delete(1)
-print([node.value for node in dll])
+# dll.delete(1)
+# print([node.value for node in dll])
 # dll.traverDll()
 # dll.reversetraverDll()
 # dll.search(32)
 # dll.delete_all_dll()
+
+# Practice 27-july-2025
+
+class Node:
+    def __init__(self, value):
+        self.next = None
+        self.previous = None
+        self.value = value
+
+
+class DoubleLinkList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+
+    def createDLL(self, value):
+        node = Node(value)
+        self.head = node
+        self.tail = node
+        return node
+
+
+
+    def traverse(self):
+        if not self.head:
+            print("empty")
+        node = self.head
+        while node:
+            print(node.next)
+            node = node.next
+
+
+    def insert_dll(self, location, value):
+        if self.head is None:
+            node = self.createDLL(value)
+            return node
+
+        new_node = Node(value)
+        if location == 0:
+            node=self.head
+            new_node.next = node
+            self.head = new_node
+            node.previous=new_node
+            return new_node
+
+        elif location == 1:
+            new_node.next = None
+            new_node.previous = self.tail
+            self.tail.next = new_node
+            self.tail=new_node
+            return new_node
+
+        else:
+            index = 0
+            current_node = self.head
+            while index < location:
+                current_node=current_node.next
+                index += 1
+
+            new_node.next = current_node
+            new_node.previous=current_node.previous
+            current_node.previous.next= new_node
+            current_node.previous=new_node
+            return new_node
+
+    def traverse_dll(self):
+        list_of_nodes= list()
+        node = self.head
+        while node:
+            # print(node.value)
+            list_of_nodes.append(node.value)
+            node=node.next
+        return list_of_nodes
+
+    def reverse_traverse_dll(self):
+        node = self.tail
+        reverse_list=list()
+        while node:
+            reverse_list.append(node.value)
+            node = node.previous
+        return reverse_list
+
+    def search_dll(self,value):
+        node = self.head
+        index = 0
+        while node:
+            if node.value == value:
+                print(node.value)
+                print(f"found at {index}")
+                return node
+            node = node.next
+            index += 1
+
+    def delete_dll(self,location):
+        if self.head is None:
+            return f"nothing to delete"
+
+        if location == 0:
+            node = self.head
+            self.head = node.next
+            node.next.previous = self.head
+            return f"sdd"
+
+        elif location == 1:
+            last = self.tail
+            self.tail = last.previous
+            last.previous.next = None
+            return f"asd"
+
+        else:
+            index = 0
+            tempnode = self.head
+            while index < location -1:
+                tempnode = tempnode.next
+                index += 1
+            tempnode.next = tempnode.next.next
+            tempnode.next.previous = tempnode
+
+
+
+DLL = DoubleLinkList()
+DLL.createDLL(4)
+DLL.insert_dll(0,5)
+DLL.insert_dll(1,6)
+DLL.insert_dll(2,7)
+DLL.insert_dll(3,8)
+DLL.insert_dll(1,9)
+
+print(DLL.traverse_dll())
+DLL.delete_dll(3)
+print(DLL.traverse_dll())
+
+print(DLL.reverse_traverse_dll())
+DLL.search_dll(8)
+
+print([node.value for node in DLL])
